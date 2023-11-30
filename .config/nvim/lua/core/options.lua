f536ec86d4f36addeaa3ccbc8be4c986fb04d1a7
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2023-11-30 12:19:35 +0000

    Neovim: Updating general options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,19 +1,28 @@
 local g = vim.g       -- Global variables
 local opt = vim.opt   -- Set options
 
+-- General
+opt.mouse = 'a'                       -- Enable mouse support
+opt.swapfile = false                  -- Don't use swapfile
+opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
+
 -- UI
-opt.number = true			        -- Show line number
-opt.relativenumber = true		    -- Show Relative Line numbers
-opt.showmatch = true			    -- Highlight matching parenthesis
-opt.ignorecase = true       		-- Ignore case letters when search
-opt.smartcase = true        		-- Ignore lowercase for the whole pattern
-opt.linebreak = true        		-- Wrap on word boundary
+opt.number = true			      -- Show line number
+opt.relativenumber = true   -- Show Relative Line numbers
+opt.showmatch = true		    -- Highlight matching parenthesis
+opt.ignorecase = true    		-- Ignore case letters when search
+opt.smartcase = true    		-- Ignore lowercase for the whole pattern
+opt.linebreak = true    		-- Wrap on word boundary
+opt.splitright = true       -- Vertical split to the right
+opt.splitbelow = true       -- Horizontal split to the bottom
+opt.laststatus=3            -- Set global statusline
+
 
 -- Tabs, indent
-opt.expandtab = true        		-- Use spaces instead of tabs
-opt.shiftwidth = 2          		-- Shift 2 spaces when tab
-opt.tabstop = 2             		-- 1 tab == 2 spaces
-opt.smartindent = true      		-- Autoindent new lines
+opt.expandtab = true     		-- Use spaces instead of tabs
+opt.shiftwidth = 2       		-- Shift 2 spaces when tab
+opt.tabstop = 2          		-- 1 tab == 2 spaces
+opt.smartindent = true   		-- Autoindent new lines
 
 -- Disable builtin plugins
 local disabled_built_ins = {
