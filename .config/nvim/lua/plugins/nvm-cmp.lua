530411c9b758ce13679ea6967eba3ffd61b9b398
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2024-02-02 10:44:57 +0000

    Neovim: updating keymaps

--- .config/nvim/lua/plugins/nvm-cmp.lua
+++ .config/nvim/lua/plugins/nvm-cmp.lua
@@ -4,5 +4,6 @@ cmp.setup({
   -- custom keybindings
   mapping = cmp.mapping.preset.insert({
     ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-.>'] = cmp.mapping.complete(),
   })
 })
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,24 +1,23 @@
-local builtin = require('telescope.builtin')
-local telescope = require('telescope')
+local builtin = require("telescope.builtin")
+local telescope = require("telescope")
 
-local keymap = require('utils').keymap
+local keymap = require("utils").keymap
 
 -- Keybinds
 
 -- Search for project files
-keymap('n', '<leader>pf', builtin.find_files)
+keymap("n", "<leader>pf", builtin.find_files)
 -- Search for git project files
-keymap('n', '<C-p>', builtin.git_files)
+keymap("n", "<C-p>", builtin.git_files)
 -- Search for string in project
-keymap('n', '<leader>ps', function()
+keymap("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 -- Search for help tags
-keymap('n', '<leader>vh', builtin.help_tags)
+keymap("n", "<leader>vh", builtin.help_tags)
 
 --lsp
-keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>')
-
+keymap("n", "gr", "<cmd>Telescope lsp_references<CR>")
 
 -- Extensions
 
@@ -26,4 +25,3 @@ keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>')
 telescope.load_extension("undo")
 
 keymap("n", "<leader>u", "<cmd>Telescope undo<cr>")
-
